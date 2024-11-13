cc0bbd618a756ff6707c2bb36329700692ccbaa8
谭义洋 <690167690@qq.com>
2024-11-13 16:54:49 +0800

    feat: 更改路由模型

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 const routes = [
@@ -26,7 +30,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
